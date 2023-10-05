16b78994c48f193a0fa4da0256a9c432c634c4c6
wasta <ccw5294@psu.edu>
2023-10-05 18:25:02 -0400

    version change

--- src/cards-app.js
+++ src/cards-app.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from 'lit';
 import "@lrnwebcomponents/meme-maker/meme-maker.js";
-import "wasta-card/src/wasta-card.js";
+import "wasta-card/src/my-card.js";
 
 const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 
